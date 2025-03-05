527db7fc34f960988379227f175558d8b0096ec9
sfan5 <sfan5@live.de>
2025-03-05 20:53:52 +0100

    Add CI run on Fedora with zlib-ng for more diversity

--- util/ci/script.sh
+++ util/ci/script.sh
@@ -1,17 +1,26 @@
 #!/bin/bash -e
 
 install_linux_deps() {
-	local pkgs=(cmake libgd-dev libsqlite3-dev libleveldb-dev libpq-dev libhiredis-dev libzstd-dev)
+	local upkgs=(
+		cmake libgd-dev libsqlite3-dev libleveldb-dev libpq-dev
+		libhiredis-dev libzstd-dev
+	)
+	local fpkgs=(
+		cmake gcc-g++ gd-devel sqlite-devel libzstd-devel zlib-ng-devel
+	)
 
-	sudo apt-get update
-	sudo apt-get remove -y 'libgd3' nginx || : # ????
-	sudo apt-get install -y --no-install-recommends "${pkgs[@]}" "$@"
+	if command -v dnf; then
+		sudo dnf install --setopt=install_weak_deps=False -y "${fpkgs[@]}"
+	else
+		sudo apt-get update
+		sudo apt-get install -y --no-install-recommends "${upkgs[@]}"
+	fi
 }
 
 run_build() {
 	local args=(
 		-DCMAKE_BUILD_TYPE=Debug
-		-DENABLE_LEVELDB=1 -DENABLE_POSTGRESQL=1 -DENABLE_REDIS=1
+		-DENABLE_LEVELDB=ON -DENABLE_POSTGRESQL=ON -DENABLE_REDIS=ON
 	)
 	[[ "$CXX" == clang* ]] && args+=(-DCMAKE_CXX_FLAGS="-fsanitize=address")
 	cmake . "${args[@]}"
